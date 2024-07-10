36547c7cb5a1e5d1d7694258d1f186e2d6846e12
lecture-bisper <bisper@outlook.kr>
2024-07-10 16:52:58 +0900

    Spring 1일차 - spring MVC 게시판 : 프로젝트 생성

---
+++ board1/src/main/java/bitc/fullstack405/board1/Board1Application.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.board1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Board1Application.class, args);
+  }
+
+}
---
+++ board1/src/test/java/bitc/fullstack405/board1/Board1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bitc.fullstack405.board1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
